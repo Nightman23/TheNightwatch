0024c8def214abc979e173c10dcb3bcc312a145a
Nightman23 <99800293+Nightman23@users.noreply.github.com>
2022-02-16 20:36:42 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,2 @@
-var b = 50 + 1 - 2 / 3 * 2;  //standard arithmethic
-b++; //adds +1 (or minus --)
-b += 1; //shortcut for b = b + 1 (can be used for all arithmetic)
-b = b % 2; //remainder
+var b = 50 
 console.log(b); //show on console
-var greetings = "Hello world!\n This is Andrew\t How are you?" //escale sequence samples
-console.log(greetings)
-var response = "I'm doing great. "
-response += "How about you?" //adding strings
-console.log(response)
