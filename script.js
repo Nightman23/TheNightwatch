1834ff67121e95c4d679077ab52acffd93d0c203
Nightman23 <99800293+Nightman23@users.noreply.github.com>
2022-02-16 20:25:52 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,10 @@
-// Wait for the document to load before running the script 
-(function ($) {
-  
-  // We use some Javascript and the URL #fragment to hide/show different parts of the page
-  // https://developer.mozilla.org/en-US/docs/Web/HTML/Element/a#Linking_to_an_element_on_the_same_page
-  $(window).on('load hashchange', function(){
-    
-    // First hide all content regions, then show the content-region specified in the URL hash 
-    // (or if no hash URL is found, default to first menu item)
-    $('.content-region').hide();
-    
-    // Remove any active classes on the main-menu
-    $('.main-menu a').removeClass('active');
-    var region = location.hash.toString() || $('.main-menu a:first').attr('href');
-    
-    // Now show the region specified in the URL hash
-    $(region).show();
-    
-    // Highlight the menu link associated with this region by adding the .active CSS class
-    $('.main-menu a[href="'+ region +'"]').addClass('active'); 
-
-    // Alternate method: Use AJAX to load the contents of an external file into a div based on URL fragment
-    // This will extract the region name from URL hash, and then load [region].html into the main #content div
-    // var region = location.hash.toString() || '#first';
-    // $('#content').load(region.slice(1) + '.html')
-    
-  });
-  
-})(jQuery);
+var b = 50 + 1 - 2 / 3 * 2;  //standard arithmethic
+b++; //adds +1 (or minus --)
+b += 1; //shortcut for b = b + 1 (can be used for all arithmetic)
+b = b % 2; //remainder
+console.log(b); //show on console
+var greetings = "Hello world!\n This is Andrew\t How are you?" //escale sequence samples
+console.log(greetings)
+var response = "I'm doing great. "
+response += "How about you?" //adding strings
+console.log(response)
